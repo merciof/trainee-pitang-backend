213adbc2f477167bfb1377ac68f387d49683459a
merciof <merciofilho@gmail.com>
2022-04-20 13:31:56 -0300

    refactor appointmentModel

--- model/appointmentModel.js
+++ model/appointmentModel.js
@@ -2,13 +2,14 @@ import mongoose from "mongoose";
 
 const appointmentSchema = new mongoose.Schema(
   {
-    name: { type: String, required: [true, "Name é um valor requerido."] },
-    appointmentDate: { type: Date },
-    birthDay: { type: Date },
+    name: { type: String, required: [true, " is a required value."] },
+    appointmentDate: { type: Date, required: [true, " is a required value."] },
+    birthDay: { type: Date, required: [true, " is a required value."] },
+    accomplished: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
 
 const appointmentModel = mongoose.model("appointment", appointmentSchema);
 
-export default appointmentModel;
+export { appointmentModel };
