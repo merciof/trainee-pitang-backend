c1714b4fa665d5287cfc00006c19489a5c886396
merciof <merciofilho@gmail.com>
2022-04-22 20:05:00 -0300

    refactor

--- controller/Controller.js
+++ controller/Controller.js
@@ -1,66 +1,61 @@
+import { response, request } from "express";
 export class Controller {
   constructor(model) {
     this.model = model;
   }
 
   async index(request, response) {
-    const appointments = await this.model.find({}).lean();
-
-    response.json(appointments);
+    try {
+      const elements = await this.model.find({}).lean();
+      response.json(elements);
+    } catch (error) {
+      response.status(400).send(error.message);
+    }
   }
 
   async create(request, response) {
-    let element = null;
-
     try {
-      element = await this.model.create(request.body);
+      const element = await this.model.create(request.body);
+      response.send(element);
     } catch (error) {
       response.status(400).send(error.message);
-      return;
     }
-
-    response.send(element);
   }
 
   async read(request, response) {
-    const id = request.params.id;
-
-    const appointment = await this.model.findById(id);
-
-    if (appointment) {
-      return response.send(appointment);
+    try {
+      const element = await this.model.findById(request.params.id);
+      response.send(element);
+    } catch (error) {
+      response.status(400).send(error.message);
     }
-
-    response.status(404).send({ message: "Element does not exist" });
   }
 
   async update(request, response) {
-    const element = await this.model.findByIdAndUpdate(
-      request.params.id,
-      request.body,
-      {
-        new: true,
-      }
-    );
+    try {
+      const element = await this.model.findByIdAndUpdate(
+        request.params.id,
+        request.body,
+        {
+          new: true,
+        }
+      );
 
-    if (element) {
-      return response.send({ element });
+      response.send({ element });
+    } catch (error) {
+      response.status(400).send(error.message);
     }
-
-    response.status(404).send({ message: "Element not found" });
   }
 
   async delete(request, response) {
     try {
       const element = await this.model.findById(request.params.id);
 
-      if (element) {
-        await element.remove();
+      await element.remove();
 
-        response.json({ message: "Element Removed" });
-      }
+      response.send("Element Removed");
     } catch (error) {
-      response.status(400).json({ message: error.message });
+      response.status(400).send(error.message);
     }
   }
 }
